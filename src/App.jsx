38b74ad2568e7e96fbec0f88b254ea656178c8ea
Max <maxtwain777@gmail.com>
2023-03-20 21:28:42 +0200

    feat: add aboutpage starter

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import MainSection from "./components/MainSefction/MainSection";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import AboutPage from "./components/AboutPage/AboutPage";
 
 function App() {
   return (
     <div>
-      <MainSection />
-      <Main />
-      <Footer></Footer>
+      <AboutPage></AboutPage>
     </div>
   );
 }
