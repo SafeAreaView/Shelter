c004a7f16d9895367ac67c9efdaf764042967ac2
Max <maxtwain777@gmail.com>
2023-03-21 19:48:28 +0200

    fix: minor fixes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Shelter/",
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
+    },
+  },
 });
